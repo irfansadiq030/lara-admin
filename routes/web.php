92eb7ac7b1b9261c2fec72e4603ddb95e8484675
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2023-05-22 08:38:05 +0500

    Admin Role created
    Admin Controller created.
    Admin Model Created.
    Admin Middleware created.
    Admin Migrations created.
    registered admin middleware in kernel.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// Admin Route
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/login', [AdminController::class, 'index'])->name('login_form');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,4 +36,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
